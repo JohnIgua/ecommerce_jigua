87c0298bf6850b5b5386c0f705c968d0344f4c2a
JohnIgua <john900430@gmail.com>
2023-01-15 13:48:54 -0500

    Primer ecommerce igua

---
+++ assets/js/data/data.js
@@ -0,0 +1,85 @@
+const db = [
+  {
+    id: 1,
+    nombre: 'Libra (1L) de Arroz',
+    descripcion: 'Una (1L) libra de Arroz generico',
+    precio: 2200,
+    imagen: './assets/img/arroz-removebg-preview.png',
+    categoria: 'Canasta familiar',
+    cantidad: 10
+  },
+  {
+    id: 2,
+    nombre: 'Libra (1L) de Azucar',
+    descripcion: 'Una (1L) libra de Azucar generico',
+    precio: 2200,
+    imagen: './assets/img/azucar-removebg-preview.png',
+    categoria: 'Canasta familiar',
+    cantidad: 15
+  },
+  {
+    id: 3,
+    nombre: 'Aceite de cocina',
+    descripcion: 'Litro (1L) de aceite de cocina',
+    precio: 9000,
+    imagen: './assets/img/aceite-removebg-preview.png',
+    categoria: 'Canasta familiar',
+    cantidad: 13
+  },
+  {
+    id: 4,
+    nombre: 'Papel Higienico',
+    descripcion: 'Paca de papel Higienico',
+    precio: 10000,
+    imagen: './assets/img/higienico.png',
+    categoria: 'Canasta familiar',
+    cantidad: 16
+  },
+  {
+    id: 5,
+    nombre: 'Cuchilla Hombre',
+    descripcion: 'Paquete de cuchillas para afeitar Hombre',
+    precio: 10000,
+    imagen: './assets/img/cuchillas.png',
+    categoria: 'Canasta familiar',
+    cantidad: 24
+  },
+  {
+    id: 6,
+    nombre: 'Detergente',
+    descripcion: 'Detergente Ariel 3Kg',
+    precio: 24000,
+    imagen: './assets/img/detergente.png',
+    categoria: 'Canasta familiar',
+    cantidad: 20
+  },
+  {
+    id: 7,
+    nombre: 'Cepillo de Dientes',
+    descripcion: 'Paquete x3 C. Dientes Colgate',
+    precio: 12000,
+    imagen: './assets/img/cepillo_dientes.png',
+    categoria: 'Canasta familiar',
+    cantidad: 14
+  },
+  {
+    id: 8,
+    nombre: 'Jabon de ropa',
+    descripcion: 'Jabon en barra para lavar ropa',
+    precio: 2500,
+    imagen: './assets/img/azul.png',
+    categoria: 'Canasta familiar',
+    cantidad: 18
+  },
+  {
+    id: 9,
+    nombre: 'Paquete de pañales',
+    descripcion: 'Paquete de pañales Winnie Et. 3',
+    precio: 65000,
+    imagen: './assets/img/pañales.png',
+    categoria: 'Canasta familiar',
+    cantidad: 15
+  }
+]
+
+export default db
